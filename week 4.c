fe1627df9355dfb0468ceeb8b9628b64e1955c10
Patricia Lo <pkwlo1992@gmail.com>
2024-02-14 23:58:47 -0800

    upload practice week 4

--- week 4.c
+++ week 4.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Write a program to count the number of words in a string
 int countWords(char *input) {
     int result = 0;
     int i = 0, len = strlen(input);
@@ -20,9 +24,69 @@ int countWords(char *input) {
     return result;
 }
 
+// Write a program that takes a number from a user and stores in hte file test.txt
+void createFile(char *fileName, char *content) {
+    FILE *file;
+    file = fopen(fileName, "w");
+    fprintf(file, "%s", content);
+    fclose(file);
+}
+
+// Write a program that finds the number of lines in a text file using fgetc(). User should be prompted to enter the
+// name of the file they want to count the lines of.
+int numberOfLines(char *fileName) {
+    FILE *file;
+    int lines = 0;
+    file = fopen(fileName, "r");
+    if (file == NULL) {
+        printf("Could not open the file for reading.");
+        return 0;
+    }
+    while(fgetc(file) != EOF) {
+        if (fgetc(file) == '\n') {
+            lines++;
+        }
+    }
+    fclose(file);
+    return lines;
+}
+
+// Write a program that reads file.txt one line at a time and checks whether the sum of the first 2 numbers
+// is equal to the third
+int sumTrue (char *fileName) {
+    FILE *file;
+    char line[100];
+    int num1, num2, num3;
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if (sscanf(line, "%d %d %d", &num1, &num2, &num3) == 3) {
+            if (num1 + num2 == num3) {
+                printf("%d + %d = %d\n", num1, num2, num3);
+            } else {
+                printf("%d + %d != %d\n", num1, num2, num3);
+            }
+        } else {
+            printf("Invalid line format: %s\n", line);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
 int main() {
-    char input[] = "Hello World";
-    int result = countWords(input);
-    printf("%d", result);
+//    char input[] = "Hello World";
+//    int result = countWords(input);
+//    printf("%d", result);
+
+//    char content[100];
+//    printf("Enter a number for the file: \n");
+//    scanf("%s", content);
+//    createFile("test.txt", content);
+//    printf("Number stored successfully");
+
+//    char fileName[100];
+//    printf("Enter the file name you want to read: \n");
+//    scanf("%s", fileName);
+//    printf("The number of lines in %s is %d.\n", fileName, numberOfLines(fileName));
     return 0;
 }
